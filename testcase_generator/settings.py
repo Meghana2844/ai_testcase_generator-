04f116fa645427d9e87b63befe0e20608c2d3dc0
Meghana2844 <meghanagagana2815@gmail.com>
2026-04-03 14:13:30 +0530

    allowedhosts error1

--- testcase_generator/settings.py
+++ testcase_generator/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
-
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+''
+ALLOWED_HOSTS = ['edgelab-mbvn.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
