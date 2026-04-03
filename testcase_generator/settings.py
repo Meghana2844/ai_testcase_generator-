c29bf1a8e98bf397dff55657dd4257abaf1c59c0
Meghana2844 <meghanagagana2815@gmail.com>
2026-04-03 14:55:15 +0530

    server error2

--- testcase_generator/settings.py
+++ testcase_generator/settings.py
@@ -112,6 +112,12 @@ DATABASES = {
     )
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://edgelab-mbvn.onrender.com"
+]
+
+# Required for Render HTTPS
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
